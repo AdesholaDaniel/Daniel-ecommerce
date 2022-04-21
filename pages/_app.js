7357521fb207dd10e80c3aaa2dc027757ed86510
AdesholaDaniel <dannyjake0001@gmail.com>
2022-04-21 03:52:57 +0100

    My final commit

---
+++ pages/_app.js
@@ -0,0 +1,25 @@
+import { useEffect } from 'react';
+import { SnackbarProvider } from 'notistack';
+import '../styles/globals.css';
+import { PayPalScriptProvider } from '@paypal/react-paypal-js';
+import { StoreProvider } from '../utils/Store';
+
+function MyApp({ Component, pageProps }) {
+  useEffect(() => {
+    const jssStyles = document.querySelector('#jss-server-side');
+    if (jssStyles) {
+      jssStyles.parentElement.removeChild(jssStyles);
+    }
+  }, []);
+  return (
+    <SnackbarProvider anchorOrigin={{ vertical: 'top', horizontal: 'center' }}>
+      <StoreProvider>
+        <PayPalScriptProvider deferLoading={true}>
+          <Component {...pageProps} />
+        </PayPalScriptProvider>
+      </StoreProvider>
+    </SnackbarProvider>
+  );
+}
+
+export default MyApp;
